b94ea39f266e6a158f91279e56af3b2c6bd0e64f
baytursevval <baytursevval@gmail.com>
2023-03-23 18:37:46 +0300

    first commit

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Basic -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<!-- Mobile Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Site Metas -->
+<title>Tech Blog - Stylish Magazine Blog Template</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Site Icons -->
+<link rel="shortcut icon" href="{{asset('assets')}}/images/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon" href="{{asset('assets')}}/images/apple-touch-icon.png">
+
+<!-- Design fonts -->
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+<!-- Bootstrap core CSS -->
+<link href="{{asset('assets')}}/css/bootstrap.css" rel="stylesheet">
+
+<!-- FontAwesome Icons core CSS -->
+<link href="{{asset('assets')}}/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="{{asset('assets')}}/style.css" rel="stylesheet">
+
+<!-- Responsive styles for this template -->
+<link href="{{asset('assets')}}/css/responsive.css" rel="stylesheet">
+
+<!-- Colors for this template -->
+<link href="{{asset('assets')}}/css/colors.css" rel="stylesheet">
+
+<!-- Version Tech CSS for this template -->
+<link href="{{asset('assets')}}/css/version/tech.css" rel="stylesheet">
+
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+    @include('home._header')
+
+@include('home._firstSection')
+@show
+
+@include('home._section')
+@show
+
+@include('home._footer')
+@show
